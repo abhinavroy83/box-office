6021f44cab4ea35746507755c03cc0802ea7f203
Abhinav <abhinavroy2099@gmail.com>
2022-07-16 23:40:21 +0530

    adding event and state

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,12 +1,22 @@
-import React from 'react'
-import MainpagesLayout from '../components/MainpagesLayout'
+import React, { useState } from 'react';
+import MainpagesLayout from '../components/MainpagesLayout';
 
 const Home = () => {
+  const [input, setInput] = useState('');
+
+  const onInputchange = ev => {
+    setInput(ev.target.value)
+    // console.log(ev.target.value);
+  };
+  const onSearch=() =>{
+    
+  }
   return (
     <MainpagesLayout>
-      this is home page
+      <input type="text" onChange={onInputchange} value={input}/>
+      <button type="button" onClick=(onSearch)>search</button>
     </MainpagesLayout>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
