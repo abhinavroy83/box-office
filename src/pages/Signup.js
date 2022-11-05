83953730b64855cbcb1b5fb083ff7a0326767e37
Abhinav <abhinavroy2099@gmail.com>
2022-11-05 12:07:13 +0530

    adding sign up page

--- src/pages/Signup.js
+++ src/pages/Signup.js
@@ -1,16 +1,21 @@
-import React from 'react'
+import React from 'react';
 
 const Signup = () => {
   return (
     <div>
       <h2>sign up </h2>
       <form>
-        
-
+        <label>
+          <p>Username</p>
+          <input type="text" />
+        </label>
+        <label>
+          <p>password</p>
+          <input type="password" />
+        </label>
       </form>
-      
     </div>
-  )
-}
+  );
+};
 
-export default Signup
+export default Signup;
